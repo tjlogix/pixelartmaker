42af786f5c1979cbf5c1ffabe6330238cd3167eb
tjlogix <107503745+tjlogix@users.noreply.github.com>
2022-06-17 00:10:54 +0000

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,14 +1,20 @@
+//select size input
 const gridHeight = document.getElementById("inputHeight");
 const gridWidth = document.getElementById("inputWidth");
 const grid = document.getElementById("pixelCanvas");
-const gridColor = document.getElementById("colorPicker");
 const form = document.getElementById("sizePicker");
+//select color input
+const gridColor = document.getElementById("colorPicker");
 
+// user clicks on submit button and calls function makeGrid()
 form.addEventListener(
     'submit',
     function(e) {
+        //remove previous grid
         grid.innerHTML = "";
+        //save curent state
         e.preventDefault();
+        //call function
         makeGrid();
 
     });
@@ -16,12 +22,18 @@ form.addEventListener(
 grid.addEventListener(
     'click',
     function(e) {
+        //select pixel
         if (e.target.nodeName === 'TD') {
             e.target.style.backgroundColor = gridColor.value
         }
     }
 )
 
+/**
+ * @desc create a grid of squares 
+ * @param int $gridWidth - number of squares representing the width of the grid
+ * @param int $gridHeight - number of squares representing the height of the grid
+ */
 
 function makeGrid() {
     for (let i = 0; i < gridHeight.value; i++) {
